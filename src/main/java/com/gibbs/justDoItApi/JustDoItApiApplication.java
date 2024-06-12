14e4dc6af68f1575e79b341bd53c1431f0cb365e
Gilbert Baracka <gilbert.baracka@gmail.com>
2024-06-11 19:01:16 -0600

    first commit

---
+++ src/main/java/com/gibbs/justDoItApi/JustDoItApiApplication.java
@@ -0,0 +1,13 @@
+package com.gibbs.justDoItApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustDoItApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustDoItApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gibbs/justDoItApi/JustDoItApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gibbs.justDoItApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustDoItApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
